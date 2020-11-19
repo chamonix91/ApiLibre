3e50cf1ec7a8938385dc3e62d8e31f8b616644bf
DESKTOP-DFLEMQ3\pc <chamseddine.bezzaouia@gmail.com>
2020-11-19 12:05:04 +0100

    Pool 1.3

--- src/Controller/Api/ApiPoolController.php
+++ src/Controller/Api/ApiPoolController.php
@@ -19,7 +19,6 @@ use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 
-
 /**
  * @Route("/pool")
  */
@@ -57,26 +56,25 @@ class ApiPoolController extends AbstractController
 
         $departures = $this->getDoctrine()
             ->getRepository(Departure::class)
-            ->findByCompany(array($date_format ,$idcompany));
+            ->findByCompany(array($date_format, $idcompany));
 
 
         $data = array();
-        foreach ($departures as $departure){
+        foreach ($departures as $departure) {
             $date_departure = $departure->getDatedeparture();
 
-            if ($date_format == $date_departure->format('d-m-Y')){
+            if ($date_format == $date_departure->format('d-m-Y')) {
 
                 $departure->setPool($pool);
                 $em->persist($departure);
                 $em->flush();
 
-            $data[] = [
-                'departure' => $departure->getId(),
-            ];
+                $data[] = [
+                    'departure' => $departure->getId(),
+                ];
             }
 
         }
-
 
         return new JsonResponse($data, 200);
 
@@ -102,9 +100,9 @@ class ApiPoolController extends AbstractController
 
         $pools = $this->getDoctrine()
             ->getRepository(Pool::class)
-            ->findBy(array('company'=> $company));
+            ->findBy(array('company' => $company));
 
-        foreach ($pools as $pool){
+        foreach ($pools as $pool) {
             $mylastpool = $pool;
         }
 
@@ -113,9 +111,9 @@ class ApiPoolController extends AbstractController
         $date = new \DateTime('now');
         $date_format = $date->format('y-m-d');
 
-        if ($date_format == $date_pool_format){
+        if ($date_format == $date_pool_format) {
 
-            $return = true ;
+            $return = true;
 
         }
 
@@ -137,24 +135,24 @@ class ApiPoolController extends AbstractController
     {
 
 
-        $pool =  $this->getDoctrine()
+        $pool = $this->getDoctrine()
             ->getRepository(Pool::class)
-            ->find(array('id'=> $id));
+            ->find(array('id' => $id));
 
         $departures = $this->getDoctrine()
             ->getRepository(Departure::class)
-            ->findBy(array('pool'=> $id));
+            ->findBy(array('pool' => $id));
 
-        foreach ($departures as $departure){
+        foreach ($departures as $departure) {
 
             $data[] = [
                 "agent_id" => $departure->getAgent()->getId(),
                 "agent_firstname" => $departure->getAgent()->getFirstname(),
                 "agent_lastname" => $departure->getAgent()->getlastname(),
                 "agent_tel" => $departure->getAgent()->getTel(),
-                "isconfirmed"=> $departure->getIsConfirmed(),
+                "isconfirmed" => $departure->getIsConfirmed(),
                 "date" => $departure->getDatedeparture()->format('d-m-Y H:i:s'),
-                "destination" =>$departure->getDestination(),
+                "destination" => $departure->getDestination(),
 
             ];
         }
@@ -175,9 +173,26 @@ class ApiPoolController extends AbstractController
      */
     public function confirmPool(SerializerInterface $serializer, Request $request, $id)
     {
-        $pool =  $this->getDoctrine()
+        $today = new \DateTime('now');
+        $today_format = $today->format('y-m-d');
+        $pool = $this->getDoctrine()
             ->getRepository(Pool::class)
-            ->find(array('id'=> $id));
+            ->find(array('id' => $id));
+
+        $company = $pool->getCompany()->getId();
+
+        $departures = $this->getDoctrine()
+            ->getRepository(Departure::class)
+            ->findBy(array("company" => $company));
+
+        foreach ($departures as $departure) {
+            $departureDate = $departure->getDatedeparture();
+            $departureDate_format = $departureDate->format('y-m-d');
+            if ($departureDate_format == $today_format && $departure->getIsConfirmed() == 1) {
+                $departure->setPool($pool);
+            }
+        }
+
 
         $pool->setConfirmed(true);
         $em = $this->getDoctrine()->getManager();
@@ -207,49 +222,45 @@ class ApiPoolController extends AbstractController
 
         $pools = $this->getDoctrine()
             ->getRepository(Pool::class)
-            ->findBy(array("company"=> $idcompany));
+            ->findBy(array("company" => $idcompany));
 
-
-
-        foreach ($pools as $pool){
+        foreach ($pools as $pool) {
             $idpool = $pool->getId();
             $departures = $this->getDoctrine()->getRepository(Departure::class)
-                ->findBy(array('pool'=> $idpool));
+                ->findBy(array('pool' => $idpool));
 
             $status = false;
             $today = new \DateTime('now');
             $today_format = $today->format('y-m-d');
             $poolDate = $pool->getDatepool();
             $poolDate_format = $poolDate->format('y-m-d');
-            if ($poolDate_format == $today_format){
-                $status = true ;
-            }
-            else{
+            if ($poolDate_format == $today_format) {
+                $status = true;
+            } else {
                 $status = false;
             }
 
-            $agents =array();
-            foreach ($departures as $departure){
-                $agents[] =  [
+            $agents = array();
+            foreach ($departures as $departure) {
+                $agents[] = [
                     "agent_id" => $departure->getAgent()->getId(),
                     "agent_firstname" => $departure->getAgent()->getFirstname(),
                     "agent_lastname" => $departure->getAgent()->getlastname(),
                     "agent_tel" => $departure->getAgent()->getTel(),
-                    "isconfirmed"=> $departure->getIsConfirmed(),
+                    "isconfirmed" => $departure->getIsConfirmed(),
                     "date" => $departure->getDatedeparture()->format('d-m-Y H:i:s'),
-                    "destination" =>$departure->getDestination(),
-
+                    "destination" => $departure->getDestination(),
                 ];
             }
-                $data[] = [
+            $data[] = [
 
-                    "id" => $pool->getId(),
-                    "date" => $pool->getDatepool()->format('d-m-Y H:i:s'),
-                    "confirmed" =>$pool->getConfirmed(),
-                    "affected" => $pool->getAffected(),
-                    "istoday" => $status,
-                    "agents" => $agents
-                ];
+                "id" => $pool->getId(),
+                "date" => $pool->getDatepool()->format('d-m-Y H:i:s'),
+                "confirmed" => $pool->getConfirmed(),
+                "affected" => $pool->getAffected(),
+                "istoday" => $status,
+                "agents" => $agents
+            ];
 
 
         }
@@ -273,18 +284,69 @@ class ApiPoolController extends AbstractController
         $today_format = $today->format('y-m-d');
         $poolDate = $pool->getDatepool();
         $poolDate_format = $poolDate->format('y-m-d');
-        if ($poolDate_format == $today_format){
-            $status = true ;
-        }
-        else{
+        if ($poolDate_format == $today_format) {
+            $status = true;
+        } else {
             $status = false;
         }
 
 
-
-        return new JsonResponse(['status'=> $status], 200);
+        return new JsonResponse(['status' => $status], 200);
     }
 
+
+    //////////////////////////////////////////
+    /////////  GET CONFIRMED POOLS  //////////
+    //////////////////////////////////////////
+
+    /**
+     * @Route( "/confirmedpools", name="api_get_confirmd_pools", methods={"GET"})
+     * @param SerializerInterface $serializer
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function confirmedPools(SerializerInterface $serializer, Request $request)
+    {
+        $today = new \DateTime('now');
+        $today_format = $today->format('y-m-d');
+        $pools = $this->getDoctrine()
+            ->getRepository(Pool::class)
+            ->findBy(array('confirmed' => "1"));
+
+        foreach ($pools as $pool) {
+            $idpool = $pool->getId();
+            $poolDate = $pool->getDatepool();
+            $poolDate_format = $poolDate->format('y-m-d');
+            if ($poolDate_format == $today_format) {
+                $departures = $this->getDoctrine()->getRepository(Departure::class)
+                    ->findBy(array('pool' => $idpool));
+
+                $agents = array();
+                foreach ($departures as $departure) {
+                    $agents[] = [
+                        "agent_id" => $departure->getAgent()->getId(),
+                        "agent_firstname" => $departure->getAgent()->getFirstname(),
+                        "agent_lastname" => $departure->getAgent()->getlastname(),
+                        "agent_tel" => $departure->getAgent()->getTel(),
+                        "isconfirmed" => $departure->getIsConfirmed(),
+                        "date" => $departure->getDatedeparture()->format('d-m-Y H:i:s'),
+                        "destination" => $departure->getDestination(),
+                    ];
+                }
+                $data[] = [
+
+                    "id" => $pool->getId(),
+                    "date" => $pool->getDatepool()->format('d-m-Y H:i:s'),
+                    "confirmed" => $pool->getConfirmed(),
+                    "affected" => $pool->getAffected(),
+                    "agents" => $agents
+                ];
+            }
+
+        }
+
+        return new JsonResponse($data, 200);
+    }
 
 
 }
